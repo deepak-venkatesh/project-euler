895b8837018dfa8170d7282277bb7b05fce67754
Deepak Venkatesh <41579969+deepak-venkatesh@users.noreply.github.com>
2025-09-27 16:36:42 -0700

    Update euler0001.lisp

--- euler0001.lisp
+++ euler0001.lisp
@@ -1,7 +1,11 @@
 ;; Project Euler Problem 1
 
+					; 0.000036 seconds of total run time (0.000036 user, 0.000000 system)
+
+
 (defun sum-three-five (n)
   (loop for i from 1 to n
 	when (or (zerop (mod i 3))
 		 (zerop (mod i 5)))
 	  sum i))
+
